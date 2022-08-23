d9cb7015b26af60c4e4c497223546a0898c61b9e
Peter Williams <peter@newton.cx>
2022-08-22 23:54:21 -0400

    tectonic.sh: forward args if provided

--- tectonic.sh
+++ tectonic.sh
@@ -5,6 +5,6 @@ set -xeuo pipefail
 (cd ~/sw/tex/tectonic && cargo build --release)
 cd template
 rm -rf build/default ../app/src/ttw
-~/sw/tex/tectonic/target/release/tectonic -X build
+~/sw/tex/tectonic/target/release/tectonic -X build "$@"
 mkdir -p ../app/src/ttw/
 cp build/default/* ../app/src/ttw/
